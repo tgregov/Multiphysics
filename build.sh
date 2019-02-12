1c621b59d41e9060f21ef787799c90b2fe2c558f
ImperatorS79 <fevrier.simon@gmail.com>
2019-02-12 22:00:09 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 wget http://gmsh.info/bin/Linux/gmsh-4.1.4-Linux64-sdk.tgz
 tar -xf gmsh-4.1.4-Linux64-sdk.tgz 
-cd gmsh-4.1.4-Linux64-sdk/include
-rm -rf gmsh.h
-mv gmsh.h_cwrap gmsh.h
-cd ../ 
+cd gmsh-4.1.4-Linux64-sdk/
 export PATH=${PWD}/bin:${PWD}/lib:${PATH}
 export INCLUDE=${PWD}/include:${INCLUDE}
 export LIB=${PWD}/lib:${LIB}
