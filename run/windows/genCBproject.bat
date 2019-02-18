e942e212874d65a1d2d99ab50ae67f3ce3fae04e
ImperatorS79 <fevrier.simon@gmail.com>
2019-02-18 10:06:01 +0100

    Add .bat for CB under windows, rename mac macOS (hardware -> software)

---
+++ run/windows/genCBproject.bat
@@ -0,0 +1,42 @@
+:: This file is intended to generate debug and release Code::Blocks project under windows.
+:: Please replace the windows compiler by the latest 64 bits compiler from here
+:: -> https://sourceforge.net/projects/mingw-w64/files/latest/download
+:: How to create a new compile profile in Code::Blocks
+:: -> https://medium.com/@yzhong.cs/code-blocks-compile-64-bit-under-windows-with-mingw-w64-79101f5bbc02
+:: Do not forget to change the CB project compiler when you launch the .cbp !
+
+@ECHO OFF
+
+set GMSHSDK=C:\Program Files (x86)\CodeBlocks\gmsh-4.1.4-Windows64-sdk
+
+:: where is gmsh.exe and gmsh-**.dll ? (HINT: copy gmsh-**.dll to the bin folder)
+set PATH=%GMSHSDK%\bin;%GMSHSDK%\lib;%PATH%
+:: where is gmsh.h ? (rename gmsh.h_cwrap => gmsh.h)
+set INCLUDE=%GMSHSDK%\include;%INCLUDE%
+:: where is gmsh.lib ?
+set LIB=%GMSHSDK%\lib;%LIB%
+:: where is gmsh.py ? (required only if you want to use the python API)
+set PYTHONPATH=%GMSHSDK%\lib;%PYTHONPATH%
+
+cd ../../
+
+rd /s /q build
+mkdir build
+cd build
+
+mkdir Release
+cd Release
+cmake ../../ -DCMAKE_BUILD_TYPE=Release  -G "CodeBlocks - MinGW Makefiles"
+copy "%GMSHSDK%\bin\gmsh-4.1.dll" "%cd%\bin"
+xcopy /E /I "../../Geometry" "%cd%\bin\Geometry"
+
+cd ../
+
+mkdir Debug
+cd Debug
+cmake ../../ -DCMAKE_BUILD_TYPE=Release  -G "CodeBlocks - MinGW Makefiles"
+copy "%GMSHSDK%\bin\gmsh-4.1.dll" "%cd%\bin"
+xcopy /E /I "../../Geometry" "%cd%\bin\Geometry"
+
+cd ../../
+PAUSE
