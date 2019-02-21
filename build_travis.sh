53849fe423e14fa3f8484488a126622c897dd6a9
tgregov <36114197+tgregov@users.noreply.github.com>
2019-02-21 15:35:01 +0100

    Update build_travis.sh
    Test for Eigen issue

--- build_travis.sh
+++ build_travis.sh
@@ -7,6 +7,11 @@ export INCLUDE=${PWD}/include:${INCLUDE}
 export LIB=${PWD}/lib:${LIB}
 export PYTHONPATH=${PWD}/lib:${PYTHONPATH}
 cd ../
+wget http://bitbucket.org/eigen/eigen/get/3.3.7.tar.gz
+tar -xf 3.3.7.tar.gz
+cd ./eigen-eigen-323c052e1731
+export INCLUDE=${PWD}:${INCLUDE}
+cd ../
 git clone https://github.com/tgregov/Multiphysics.git
 cd Multiphysics/  
 mkdir build
