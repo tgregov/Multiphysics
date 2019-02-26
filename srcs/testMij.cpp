d9b0fb005d358c7cae5e7e38ad300dc4abd7d228
Thomas Gregov <thomasgregov@gmail.com>
2019-02-26 11:31:51 +0100

    First try of a construction of [M]

--- srcs/testMij.cpp
+++ srcs/testMij.cpp
@@ -104,6 +104,98 @@ bool testMij(const std::string& fileName)
     // - pts contains for each element the (x, y, z) coordinates in the 
     //   physical frame of the GP => we have 4*3 = 12 components
 
+    // -------------------------------------------------------------------------
+    //                          TEST FOR THE MATRIX M 
+    // -------------------------------------------------------------------------
+    // We need to compute:
+    // sum_k{w_k*l_i(x_k)*l_j(x_k)*[detJ](x_k)}
+
+    // parameters
+    unsigned int nGP = intpts.size()/4; //maybe short
+    unsigned int nSF = bf.size()/nGP;
+    unsigned int nE = det.size()/nGP;
+
+    std::cout << "number of GP: " << nGP << std::endl; 
+    std::cout << "number of SF: " << nSF << std::endl; 
+    std::cout << "number of E: " << nE << std::endl; 
+
+    // T^(k)_{ij}:
+    // k is the GP
+    // {ij} is the l_i*l_j
+    std::vector<std::vector<double>> T;
+
+    for(unsigned int k = 0; k < nGP; k++)
+    {
+        std::vector<double> t;
+
+        for(unsigned int i = 0; i < nSF; i++)
+        {
+            for(unsigned int j = i; j < nSF; j++)
+            {
+               t.push_back(intpts[4*k + 3]*bf[nGP*i + k]*bf[nGP*j + k]);
+            }
+        }
+
+        T.push_back(t);
+    }
+
+    // E^(e)_{ij}
+    // e is the element
+    // {ij} is the l_i*l_j
+    std::vector<std::vector<double>> E;
+
+    for(unsigned int elm = 0; elm < nE; elm++)
+    {
+        std::vector<double> e;
+
+        for(unsigned int l = 0; l < nSF*(nSF+1)/2; l++)
+        {
+            double sum = 0;
+            for(unsigned int k = 0; k < nGP; k++)
+            {
+                sum += T[k][l]*det[elm*nGP + k];
+            }
+
+            e.push_back(sum);
+        }
+
+        E.push_back(e);
+    }
+
+    // assembly of the matrix M_ij
+    // temporary => later we will use Eigen
+    std::vector<std::vector<double>> M(nE*nSF, std::vector<double>(nE*nSF, 0.0));
+
+
+    for(unsigned int elm = 0; elm < nE; elm++)
+    {   
+        unsigned int i = 0;
+        unsigned int j = 0;
+        for(unsigned int l = 0; l < nSF*(nSF+1)/2; l++)
+        {
+            M[i + elm*nSF][j + elm*nSF] = E[elm][l];
+            if(i != j)
+            {
+                M[j + elm*nSF][i + elm*nSF] = E[elm][l];
+            }
+
+            j += 1;
+            if(j == nSF)
+            {
+                i += 1;
+                j = i;
+            }
+        }
+    }  
+
+    for(size_t i = 0; i < M.size(); i++)
+    {
+        for(size_t j = 0; j < M[i].size(); j++)
+        {
+            std::cout << "| " << M[i][j] << " ";
+        }
+        std::cout << "|" << std::endl;
+    }
 
     return true;
 }
