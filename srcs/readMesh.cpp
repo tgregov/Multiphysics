8d49a0722999ecf76384e9b40fda32e1d6de0f4b
Thomas Gregov <thomasgregov@gmail.com>
2019-03-01 23:23:21 +0100

    Normal computation comments
    I have reviewed @imperators79 contribution to the normal computation. Evrything seems ok (and in particular I have not modified anything); I have just added comments.
    
    Nonetheless, I have 3 remarks:
    (i) The current code does not handle the case where we have more than 2 nodes per element (and for high order elements we defenitely will get more than 2 nodes !). I don't know if taking that into account is possible for us at the current moment.
    (ii) The efficiency could be improved, by considering that almost every normal is computed twice (adjacent elements) => maybe we could think of something to take that into account
    (iii) The problem with the clockwise/counter-clockwise may be solved by considering the 3rd component of the jacobian of the element, i.e. wheter or not the element was flipped. Maybe then, there is a rule for the order in which the nodes per edge are organized.

--- srcs/readMesh.cpp
+++ srcs/readMesh.cpp
@@ -41,6 +41,7 @@ bool readMesh(MeshParams& meshParams, const std::string& fileName,
     gmsh::option::setNumber("General.Terminal", 1);
     gmsh::open(fileName);
 
+    // loop over the dimension i to get the maximum element dimension in the mesh
     for(unsigned short i = 1 ; i <= 3 ; ++i)
     {
         std::vector<int> eleTypes;
@@ -55,89 +56,129 @@ bool readMesh(MeshParams& meshParams, const std::string& fileName,
                 break;
             default:
                 gmsh::logger::write("Hybrid meshes not handled in this example!",
-                                "error");
+                                    "error");
 
                 gmsh::finalize();
                 return false;
         }
     }
 
+    // we currently only handle 2D meshes
     if(meshParams.elementDim != 2)
     {
-        gmsh::logger::write("Only 2D meshes handled currently!",
-                                "error");
+        gmsh::logger::write("Only 2D meshes handled currently!", "error");
 
         gmsh::finalize();
         return false;
     }
 
-    // get basis functions
-    int numComp;
 
-    gmsh::model::mesh::getBasisFunctions(eleType2D, intScheme, basisFuncType,
-                                         meshParams.intPoints, numComp, 
-                                         meshParams.basisFunc);
-    gmsh::model::mesh::getBasisFunctions(eleType2D, intScheme, basisFuncGradType,
-                                         meshParams.intPoints, numComp, 
-                                         meshParams.basisFuncGrad);
+    // I. BASIS FUNCTIONS & JACOBIANS
+
+    int numComp;
+    // get basis functions and their gradients
+    gmsh::model::mesh::getBasisFunctions(meshParams.elementType, intScheme, 
+                                            basisFuncType, meshParams.intPoints, 
+                                            numComp, meshParams.basisFunc);
+    gmsh::model::mesh::getBasisFunctions(meshParams.elementType, intScheme, 
+                                            basisFuncGradType, meshParams.intPoints, 
+                                            numComp, meshParams.basisFuncGrad);
 
     std::vector<std::pair<int, int>> entities;
     gmsh::model::getEntities(entities, 2);
     int c = entities[0].second; // c is the tag of the surface
 
-    // Get the Jacobians information for the 2D triangular elements
+    // get the jacobians
     std::vector<double> pts;
-    gmsh::model::mesh::getJacobians(eleType2D, intScheme, meshParams.jacobian, 
-                                    meshParams.determinant, pts, c);
+    gmsh::model::mesh::getJacobians(meshParams.elementType, intScheme, 
+                                    meshParams.jacobian, meshParams.determinant, pts,
+                                    c);
 
-    // TO DO: what if there are more than one surface ? -> handle that case
+    // get the number of Gauss points and shape functions
+    meshParams.nGP = meshParams.intPoints.size()/4;
+    meshParams.nSF = meshParams.basisFunc.size()/meshParams.nGP;
+
+
+    // II. NORMALS
+    // [TO DO]: handle the case where there is more than one surface
     for (std::size_t i = 0; i < entities.size(); i++)
     {
         // get the tag of the current surface
         int entityTag = entities[i].second;
 
+        // get the tags of all the elements
         std::vector<int> nodeTags;
-        gmsh::model::mesh::getElementsByType(meshParams.elementType, meshParams.elementTags, nodeTags, entityTag);
+        gmsh::model::mesh::getElementsByType(meshParams.elementType, 
+                                                meshParams.elementTags, nodeTags, 
+                                                entityTag);
 
+        // get the barycenters of all the elements
         std::vector<double> baryCenters;
-        gmsh::model::mesh::getBarycenters(meshParams.elementType, entityTag, false, true, baryCenters);//When multiple entity, table of meshParams ? ^
+        gmsh::model::mesh::getBarycenters(meshParams.elementType, entityTag, false,
+                                            true, baryCenters);
+        // [Simon] When multiple entity, table of meshParams ?
 
         // get the nodes (in fact, tags) on the edges of the 2D elements
-        // -> to do so, we search over the 2D elements of type eleType2D,
-        // belonging to the entity entityTag
-        std::vector<int> nodes;  // 2nodes per edge per element
-        gmsh::model::mesh::getElementEdgeNodes(meshParams.elementType, nodes, entityTag, true);
+        // -> to do so, we search over the 2D elements of type 
+        // meshParams.elementType, belonging to the entity of tag entityTag
+        // N.B.: there are 2 nodes per edge per element
+        std::vector<int> nodes;
+        gmsh::model::mesh::getElementEdgeNodes(meshParams.elementType, nodes, 
+                                                entityTag, true);
 
+        // save the number of elements
         meshParams.nE = meshParams.elementTags.size();
 
-        double nEdgePerEl = nodes.size()/(2* meshParams.nE);
+        // compute the number of edges per element
+        double nEdgePerEl = nodes.size()/(2*meshParams.nE);
 
-        // Compute a vector of nodes per edge per element and normal per edge per element
-        // to be generalized to 1D and 3D
-        for(std::size_t j = 0; j < meshParams.nE; j++)
+        // compute a vector of nodes per edge per element and normal per edge 
+        // per element => to be generalized to 1D and 3D
+
+        // loop over the elements
+        for(std::size_t elm = 0; elm < meshParams.nE; elm++)
         {
+            // edgeList contains, for the current element, for all its edges, the 
+            // associated nodes tags
+            // normalList contains, for the current element, for all its edges, the 
+            // components of the normal
             std::vector<std::pair<int, int>> edgeList;
-            std::vector<std::vector<double>> normalList; //List of normal for an element
-            for(unsigned short k = 0 ; k < nEdgePerEl ; k++) //Normally now any element type
+            std::vector<std::vector<double>> normalList;
+
+            // loop over the edges of the current element
+            for(unsigned short k = 0 ; k < nEdgePerEl; k++)
             {
-                std::vector<double> normal; //The normal of one edge
 
-                edgeList.push_back(std::pair<int, int>(nodes[2*nEdgePerEl*j+2*k], nodes[2*nEdgePerEl*j+2*k+1]));
+                std::vector<double> normal;
+                // get the nodes of the current edge (the "2" factor comes from the 
+                // fact that there are each time 2 values for a single edge: the
+                // associated node tags)
+                edgeList.push_back(std::pair<int, int>(nodes[2*nEdgePerEl*elm + 2*k],
+                                                    nodes[2*nEdgePerEl*elm + 2*k+1]));
 
+                // get the coordinates of the first node
                 std::vector<double> coord1, parametricCoord1;
-                gmsh::model::mesh::getNode(edgeList[k].first,  coord1, parametricCoord1);
+                gmsh::model::mesh::getNode(edgeList[k].first, coord1, 
+                                            parametricCoord1);
 
+                // get the coordinates of the second node
                 std::vector<double> coord2, parametricCoord2;
-                gmsh::model::mesh::getNode(edgeList[k].second,  coord2, parametricCoord2);
+                gmsh::model::mesh::getNode(edgeList[k].second, coord2, 
+                                            parametricCoord2);
 
-                double nx = coord2[1]-coord1[1];  //Not general, 2D case, to be genralized for 1D and 3D
-                double ny = coord1[0]-coord2[0];
+                // compute the normal
+                // if A:(x1, y1) and B:(x2, y2), then AB = (x2 - x1, y2 - y1) and a 
+                // normal is given by n = (y2 - y1, x1 - x2)
+                // [TO DO] generalize for 1D and 3D
+                double nx = coord2[1] - coord1[1];
+                double ny = coord1[0] - coord2[0];
                 double norm = sqrt(ny*ny + nx*nx);
 
-                //Unfortunately, nodes per edge in nodes vector are not always in the same order
-                //clockwise vs anticlockwise
-                double vx = baryCenters[3*j]-(coord2[0]+coord1[0])/2;
-                double vy = baryCenters[3*j+1]-(coord2[1]+coord1[1])/2;
+                // unfortunately, nodes per edge in nodes vector are not always in 
+                // the same order (clockwise vs anticlockwise) => we need to check
+                // the orientation
+                double vx = baryCenters[3*elm] - (coord2[0] + coord1[0])/2;
+                double vy = baryCenters[3*elm + 1] - (coord2[1] + coord1[1])/2;
 
                 if(nx*vx + ny*vy > 0)
                 {
@@ -145,37 +186,45 @@ bool readMesh(MeshParams& meshParams, const std::string& fileName,
                     ny = -ny;
                 }
 
+                // normalize the normal components
                 normal.push_back(nx/norm);
                 normal.push_back(ny/norm);
 
-                normalList.push_back(normal); //we add the normal the the list of element's normal
+                // add the normal to the list of normals of the current element
+                normalList.push_back(normal);
             }
 
+            // meshParams.nodes is a vector of vector of a pair: it lists for all the
+            // elements, then for all the edges the pair of node tags
+            // meshParams.normals is a vector of vector of vector: it lists for all 
+            // the elements, then for all the edges the components of the exterior
+            // normal
             meshParams.nodes.push_back(edgeList);
-            meshParams.normals.push_back(normalList); //we add the list of normal of one element to the full list of normal.
+            meshParams.normals.push_back(normalList);
         }
 
-        //Just read what we have found
+        // [TO REMOVE] display the normals to check that it works
         for(std::size_t j = 0; j < meshParams.nE; j++)
         {
-            std::cout<<"Element: "<<meshParams.elementTags[j]<<std::endl;
+            std::cout<<"Element: " << meshParams.elementTags[j] << std::endl;
             for (unsigned short i = 0 ; i < meshParams.normals[j].size() ; i++ )
             {
-                std::cout<<"\tEdge: "<<i<<std::endl;
-                std::cout<<"\t\tNormal: (";
+                std::cout << "\tEdge: " << i << std::endl;
+                std::cout << "\t\tNormal: (";
                 for (unsigned short k = 0 ; k < meshParams.normals[j][i].size() ; k++ )
                 {
-                    std::cout<<meshParams.normals[j][i][k]<<", ";
+                    std::cout << meshParams.normals[j][i][k];
+                    if(k != meshParams.normals[j][i].size() - 1)
+                    { 
+                        std::cout << ", ";
+                    }
                 }
-                std::cout<<")"<<std::endl;
+                std::cout << ")" << std::endl;
             }
         }
     }
 
     gmsh::finalize();
-
-    meshParams.nGP = meshParams.intPoints.size()/4;
-    meshParams.nSF = meshParams.basisFunc.size()/meshParams.nGP;
 
     return true;
 }
