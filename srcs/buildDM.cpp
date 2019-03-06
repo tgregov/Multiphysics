f0e3025fd9b49e888b3285a7d66f64a505da4114
Thomas Gregov <thomasgregov@gmail.com>
2019-03-06 19:49:48 +0100

    Correct segmentation fault
    * basicFunc -> basisFuncInferior
    * [s] -> push_back
    We probably still need to check manually that it is OK

--- srcs/buildDM.cpp
+++ srcs/buildDM.cpp
@@ -11,20 +11,24 @@ void buildDM(MeshParams& meshParams)
 	double lala = 0.0;
 	double lalb = 0.0;
 	unsigned int nSigma = meshParams.nSigma;
+	
 	for (unsigned int k = 0 ; k < meshParams.nGPInf ; k++)
 	{
 	    lala += meshParams.intPointsInferior[4*k + 3]
-	                    *meshParams.basisFunc[meshParams.nSFInf*k]
-	                    *meshParams.basisFunc[meshParams.nSFInf*k];
+	                    *meshParams.basisFuncInferior[meshParams.nSFInf*k]
+	                    *meshParams.basisFuncInferior[meshParams.nSFInf*k];
 
 		lalb += meshParams.intPointsInferior[4*k + 3]
-	                    *meshParams.basisFunc[meshParams.nSFInf*k]
-	                    *meshParams.basisFunc[meshParams.nSFInf*k + 1];
+	                    *meshParams.basisFuncInferior[meshParams.nSFInf*k]
+	                    *meshParams.basisFuncInferior[meshParams.nSFInf*k + 1];
 	}
+
+
 
 	for(unsigned int s = 0 ; s < meshParams.nSigma ; s++)
 	{
-		meshParams.dM[s] = Eigen::SparseMatrix<double>(meshParams.nSF,meshParams.nSF);
+		meshParams.dM.push_back(Eigen::SparseMatrix<double>(meshParams.nSF,meshParams.nSF));
+		
 		std::vector<Eigen::Triplet<double>> index;
 		index.push_back(Eigen::Triplet<double>(s, s, lala));
 		index.push_back(Eigen::Triplet<double>(s, (s+1) % nSigma, lalb));
@@ -32,6 +36,7 @@ void buildDM(MeshParams& meshParams)
 		index.push_back(Eigen::Triplet<double>((s+1) % nSigma, (s+1)%nSigma, lala));
 
 		meshParams.dM[s].setFromTriplets(index.begin(), index.end());
+		
 	}
 
 }
