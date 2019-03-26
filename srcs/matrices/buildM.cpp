661a59226f627421fce792122388de70825520a4
Thomas Gregov <thomasgregov@gmail.com>
2019-03-26 16:25:41 +0100

    Improve comments for matrices [M] and [S]
    Co-Authored-By: Joachim Marichal <jmarichal@users.noreply.github.com>

--- srcs/matrices/buildM.cpp
+++ srcs/matrices/buildM.cpp
@@ -2,54 +2,70 @@
 #include "buildM.hpp"
 
 
+// see .hpp file for description
 void buildM(const Mesh2D& mesh, Eigen::SparseMatrix<double>& invM)
 {
 
-    // the index is a vector of triplets that contains the coordinates in the [M]
-    // matrix of each ot its components; the offsetMatrix is the upper-left
-    // coordinate at which the current element matrix should be added
+    // * index: vector of triplets that contains the coordinates in the [M] matrix 
+    //          for each of its component
+    // * offsetMatrix: upper-left coordinate at which the current element matrix 
+    //          should be added
     std::vector<Eigen::Triplet<double>> index;
     unsigned int offsetMatrix = 0;
 
     // loop over the entites
-    for(unsigned int ent = 0 ; ent < mesh.entities.size() ; ++ent)
+    for(size_t ent = 0 ; ent < mesh.entities.size() ; ++ent)
     {
+
+        // current entity
         Entity2D entity = mesh.entities[ent];
 
         // loop over the elements
-        for(unsigned int elm = 0 ; elm < entity.elements.size() ; ++elm)
+        for(size_t elm = 0 ; elm < entity.elements.size() ; ++elm)
         {
+
+            // current element
             Element2D element = entity.elements[elm];
 
-            // get the properties of the current element type
-            ElementProperty elmProp = mesh.elementProperties2D.at(element.elementType2D);
+            // get the 2D properties of the current element type
+            // * prodFunc[k][i,j]: w_k*l_i*l_j evaluated at each GP
+            // * IJ[l]: components (i, j) for the index l that runs through the 
+            //          upper-half part of [M]
+            ElementProperty elmProp 
+                = mesh.elementProperties2D.at(element.elementType2D);
             std::vector<std::vector<double>> prodFunc = elmProp.prodFunc;
             std::vector<std::pair<unsigned int, unsigned int>> IJ = elmProp.IJ;
 
+            // local [M] matrix for the current element
             Eigen::MatrixXd MLocal(elmProp.nSF, elmProp.nSF);
             MLocal.setZero();
 
-            // construct the local [M] matrix for the current element
+            // construct the local [M] matrix
+            // since [M] is symmetric, we only loop over the upper-half matrix
             for(unsigned int l = 0 ; l < elmProp.nSF*(elmProp.nSF+1)/2 ; ++l)
             {
 
                 // sum over the GP
-                double sum = 0;
+                double sum = 0.0;
                 for(unsigned int k = 0 ; k < elmProp.nGP ; ++k)
                 {
+                    // M_ij = sum_k{w_k*l_i(x_k)*l_j(x_k)*det[J](x_k)}
                     sum += prodFunc[k][l]*element.determinant2D[k];
                 }
 
                 MLocal(IJ[l].first, IJ[l].second) = sum;
+
+                // if we are not on the diagonal, we also add the lower-half matrix
                 if(IJ[l].first != IJ[l].second)
                 {
                     MLocal(IJ[l].second, IJ[l].first) = sum;
                 }   
-
             }
 
             // inverse local M matrix (which is also symmetric)
             MLocal = MLocal.inverse();
+
+            // set the indices for the global [M] matrix
             for(unsigned int l = 0 ; l < elmProp.nSF*(elmProp.nSF+1)/2 ; ++l)
             {
                 index.push_back(Eigen::Triplet<double>
@@ -57,6 +73,7 @@ void buildM(const Mesh2D& mesh, Eigen::SparseMatrix<double>& invM)
                         IJ[l].second + offsetMatrix,
                         MLocal(IJ[l].first, IJ[l].second)));   
 
+                // if we are not on the diagonal, we also add the lower-half matrix
                 if(IJ[l].first != IJ[l].second)
                 {
                     index.push_back(Eigen::Triplet<double>
@@ -66,6 +83,7 @@ void buildM(const Mesh2D& mesh, Eigen::SparseMatrix<double>& invM)
                 }         
             }
 
+            // increase the offset of the local matrix
             offsetMatrix += elmProp.nSF;
         }
     }
--- srcs/matrices/buildS.cpp
+++ srcs/matrices/buildS.cpp
@@ -1,82 +1,83 @@
 #include <iostream>
 #include "buildS.hpp"
 
+
+// see .hpp for description
 void buildS(const Mesh2D& mesh, Eigen::SparseMatrix<double>& Sx,
 			Eigen::SparseMatrix<double>& Sy)
 {
 
-    // indexx, indexy are vectors of triplets that contains the coordinates in the
-    // [Sx], [Sy] matrices of each ot their components; the offsetMatrix is the
-    // upper-left coordinate at which the current element matrix should be added
+	// * indexx/indexy: vectors of triplets that contains the coordinates in the
+    // 		[Sx]/[Sy] matrices of each ot their components
+    // * offsetMatrix: upper-left coordinate at which the current element matrix 
+    //		should be added
 	std::vector<Eigen::Triplet<double>> indexx, indexy;
     unsigned int offsetMatrix = 0;
 
     // loop over the entites
-    for(unsigned int ent = 0 ; ent < mesh.entities.size() ; ++ent)
+    for(size_t ent = 0 ; ent < mesh.entities.size() ; ++ent)
     {
+
+    	// current entity
         Entity2D entity = mesh.entities[ent];
 
         // loop over the elements
-        for(unsigned int elm = 0 ; elm < entity.elements.size() ; ++elm)
+        for(size_t elm = 0 ; elm < entity.elements.size() ; ++elm)
         {
+
+        	// current element
             Element2D element = entity.elements[elm];
 
-            // get the properties of the current element type
-            ElementProperty elmProp = mesh.elementProperties2D.at(element.elementType2D);
+			// get the 2D properties of the current element type
+            // * pondFunc[k][i]: w_k*l_i evaluated at each GP
+            ElementProperty elmProp 
+            	= mesh.elementProperties2D.at(element.elementType2D);
             std::vector<std::vector<double>> pondFunc = elmProp.pondFunc;
 
-            // matrices [Sx], [Sy] for the current element, represented as vectors
+            // matrices [Sx], [Sy] for the current element, the matrices are stored 
+            // as vectors, such that S_{i,j} = [S](i*elmProp.nSF + j)
             std::vector<double> sxElm(elmProp.nSF*elmProp.nSF, 0.0);
 			std::vector<double> syElm(elmProp.nSF*elmProp.nSF, 0.0);
 
+			// sum over the GP
 			for(unsigned int k = 0 ; k < elmProp.nGP ; ++k)
 			{
 
-				// only the 2D case here; ideally it should be more general
-				// [TO CHECK] in the SDK, it is said that jacobian is stored as
-				// [Jxx, Jxy, Jxz, ...]. However, it seems that it is reversed:
-				// by manually checking, I get that jacobian[0] = dx1/dX1,
-				// jacobian[1] = dx2/dX1, etc
-				// N.B.1: maybe it depends on how we define the jacobian matrix [J]
-				// N.B.2: how to check manually ? We have our simple 4 triangles
-				// in a square geometry. We can assume a linear change of variables
-				// => 	x = (dx/dxi)*xi + (dx/deta)*eta + (dx/dzeta)*zeta + kappa_x
-				// 		y = (dy/dxi)*xi + (dy/deta)*eta + (dy/dzeta)*zeta + kappa_y
-				//		z = (dz/dxi)*xi + (dz/deta)*eta + (dz/dzeta)*zeta + kappa_z
-				// then, just take (xi, eta) in {(0, 0), (1, 0), (0, 1)} and verify
-				// that it works (the kappas correspond to simple translation)
-				// N.B.3: the basisFuncGrad seems alright by checking manually
-				double dxdxi = element.jacobian2D[9*k];
-				double dxdeta = element.jacobian2D[9*k + 3];
-				double dydxi = element.jacobian2D[9*k + 1];
-				double dydeta = element.jacobian2D[9*k + 4];
+				// only the 2D case here
+				double dxdxi 	= element.jacobian2D[9*k];
+				double dxdeta 	= element.jacobian2D[9*k + 3];
+				double dydxi 	= element.jacobian2D[9*k + 1];
+				double dydeta 	= element.jacobian2D[9*k + 4];
 
-				// necessary for the quick computation of the inverse
+				// dzdzeta component of the jacobian: +/-1
 				double sign = element.jacobian2D[9*k + 8];
 
-				// N.B.: the dets simplify in the inverse of a 2x2 matrix !
+				// the dets simplify in the inverse of a 2x2 matrix !
 				double dxidx 	= dydeta/sign;
 				double detadx 	= - dydxi/sign;
 				double dxidy 	= - dxdeta/sign;
 				double detady 	= dxdxi/sign;
 
-				for(unsigned int i = 0; i < elmProp.nSF; i++)
+				// loop over the components of the matrix
+				for(unsigned int i = 0 ; i < elmProp.nSF ; ++i)
 				{
-					for(unsigned int j = 0; j < elmProp.nSF; j++)
+					for(unsigned int j = 0 ; j < elmProp.nSF ; ++j)
 					{
 
+						// gradient of the shape functions
 						double dljdxi = elmProp.basisFuncGrad
 							[k*elmProp.nSF*3 + j*3];
 						double dljdeta = elmProp.basisFuncGrad
 							[k*elmProp.nSF*3 + j*3 + 1];
 
+						// components of the elements
 						sxElm[i*elmProp.nSF + j]
 							+= pondFunc[k][i]*(dljdxi*dxidx + dljdeta*detadx);
 						syElm[i*elmProp.nSF + j]
 							+= pondFunc[k][i]*(dljdxi*dxidy + dljdeta*detady);
 
-						// if we have calculated the sum for all the GP, we can
-						// save the computed components
+						// if we have calculated the sum for all the GP, 
+						// we can save the computed components
 						if(k == elmProp.nGP - 1)
 						{
 							indexx.push_back(Eigen::Triplet<double>
@@ -91,10 +92,12 @@ void buildS(const Mesh2D& mesh, Eigen::SparseMatrix<double>& Sx,
 				}
 			}
 
+			// increase the offset of the local matrix
 			offsetMatrix += elmProp.nSF;
 		}
    	}
 
+    // add the triplets in the sparse matrix
     Sx.setFromTriplets(indexx.begin(), indexx.end());
     Sy.setFromTriplets(indexy.begin(), indexy.end());
 }
