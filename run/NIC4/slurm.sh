71447ef593dd1c68da2d31f58479806279d19bde
ImperatorS79 <fevrier.simon@gmail.com>
2019-04-29 22:22:53 +0200

    Little correction for large mesh reading

--- run/NIC4/slurm.sh
+++ run/NIC4/slurm.sh
@@ -4,7 +4,7 @@
 #SBATCH --time=01:00:00 # hh:mm:ss
 #
 #SBATCH --ntasks=1 
-#SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=16
 #SBATCH --mem-per-cpu=1024 # megabytes 
 #SBATCH --partition=defq 
 #SBATCH --output=out.txt
@@ -13,7 +13,8 @@ module load gcc/4.9.2
 export CC=gcc
 export CXX=g++
 export FC=gfortran
-export OMP_NUM_THREADS=10
+export OMP_NUM_THREADS=16
+export OMP_CANCELLATION=true
 
 cd $HOME/Multiphysics
 srun ./build/bin/main ./Geometry/2D\ Rectangle/rectangle.msh ./Params/param.dat
