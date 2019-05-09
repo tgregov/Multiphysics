f6a818de5fea4627fd5683929ac0cbc49418dcb6
ImperatorS79 <fevrier.simon@gmail.com>
2019-05-09 12:38:03 +0200

    Use MPI_Allgatherv instead of MPI_Gatherv + MPI_Bcast
    Reduce the number of API calls by `unk`*(1+`dim`) per time step (RK1).
    
    This increase the ammount of sended data, but reduce the maximum size of sended data (d/N instead of d)

--- srcs/mpi/sendReceive.cpp
+++ srcs/mpi/sendReceive.cpp
@@ -11,20 +11,9 @@ void exchangeFlux(const Field& field, CompleteField& compField,
     {
         for(unsigned short unk = 0 ; unk < solverParams.nUnknowns ; ++unk)
         {
-            MPI_Gatherv(&field.flux[dim][unk][0], domainDiv.node[rank], MPI_DOUBLE,
-                        &compField.flux[dim][unk][0], domainDiv.node.data(),
-                        domainDiv.nodePrec.data(), MPI_DOUBLE,
-                        0, MPI_COMM_WORLD);
-        }
-    }
-
-    //Master Thread send compField.flux to everyone
-    for(unsigned short dim = 0 ; dim < mesh.dim ; ++dim)
-    {
-        for(unsigned short unk = 0 ; unk < solverParams.nUnknowns ; ++unk)
-        {
-            MPI_Bcast(&compField.flux[dim][unk][0],  mesh.nodeData.numNodes, MPI_DOUBLE,
-                      0, MPI_COMM_WORLD);
+            MPI_Allgatherv(&field.flux[dim][unk][0], domainDiv.node[rank], MPI_DOUBLE,
+                            &compField.flux[dim][unk][0], domainDiv.node.data(),
+                            domainDiv.nodePrec.data(), MPI_DOUBLE, MPI_COMM_WORLD);
         }
     }
 }
@@ -35,16 +24,8 @@ void exchangeUnk(const Field& field, CompleteField& compField,
 {
     for(unsigned short unk = 0 ; unk < solverParams.nUnknowns ; ++unk)
     {
-        MPI_Gatherv(&field.u[unk][0], domainDiv.node[rank], MPI_DOUBLE,
+        MPI_Allgatherv(&field.u[unk][0], domainDiv.node[rank], MPI_DOUBLE,
                     &compField.u[unk][0], domainDiv.node.data(),
-                    domainDiv.nodePrec.data(), MPI_DOUBLE,
-                    0, MPI_COMM_WORLD);
-    }
-
-    //Master Thread send compField.flux to everyone
-    for(unsigned short unk = 0 ; unk < solverParams.nUnknowns ; ++unk)
-    {
-        MPI_Bcast(&compField.u[unk][0], mesh.nodeData.numNodes, MPI_DOUBLE,
-                  0, MPI_COMM_WORLD);
+                    domainDiv.nodePrec.data(), MPI_DOUBLE, MPI_COMM_WORLD);
     }
 }
